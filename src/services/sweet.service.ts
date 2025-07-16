1e10e4216715c0cd24ef8704533ace26f66752e2
mdhruvil <dhruvil1808@gmail.com>
2025-07-16 16:39:46 +0530

    feat: implement getAllSweets and createSweet methods in SweetService

--- src/services/sweet.service.ts
+++ src/services/sweet.service.ts
@@ -1,5 +1,17 @@
+import type { Sweet } from "../models/sweet.model.js";
 import type { ISweetRepository } from "../repositories/sweet.repository.js";
 
 export class SweetService {
   constructor(private repository: ISweetRepository) {}
+
+  getAllSweets() {
+    return this.repository.getAll();
+  }
+
+  createSweet(sweet: Sweet): Sweet {
+    if (!sweet) {
+      throw new Error("Sweet cannot be null or undefined");
+    }
+    return this.repository.create(sweet);
+  }
 }
