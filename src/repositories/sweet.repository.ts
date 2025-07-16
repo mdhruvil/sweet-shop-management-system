44daf75120dfbf01ea54a677f3a8888a7a806d03
mdhruvil <dhruvil1808@gmail.com>
2025-07-16 16:05:37 +0530

    feat: implement `create` and `getAll` in SweetRepository

--- src/repositories/sweet.repository.ts
+++ src/repositories/sweet.repository.ts
@@ -1,3 +1,24 @@
+import type { Sweet } from "../models/sweet.model.js";
+
 export class SweetRepository {
-  constructor() {}
+  private sweets: Sweet[];
+
+  constructor() {
+    this.sweets = [];
+  }
+  create(sweet: Sweet): Sweet {
+    if (!sweet) {
+      throw new Error("Sweet cannot be null or undefined");
+    }
+
+    if (this.sweets.some((s) => s.id === sweet.id)) {
+      throw new Error(`Sweet with ID ${sweet.id} already exists`);
+    }
+
+    this.sweets.push(sweet);
+    return sweet;
+  }
+  getAll(): Sweet[] {
+    return this.sweets;
+  }
 }
